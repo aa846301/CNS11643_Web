826768b587e3a98612f0e2be21ee9d3470d05d78
aa846301 <aa846301@gmail.com>
2023-03-10 00:06:40 +0800

    Updata word_char_code Models

--- CNS11643_web/word_crud/admin.py
+++ CNS11643_web/word_crud/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from .models import word_char_code
 
 # Register your models here.
-
-
-
+#註冊models到後台
+admin.site.register(word_char_code)
--- CNS11643_web/word_crud/models.py
+++ CNS11643_web/word_crud/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class word_char_code(models.Model):
-    cns = []
-    big5 = []
-    unicode = [] 
-    
+    cns_code = models.CharField(max_length=7,blank=False,null=False)
+    unicode_code = models.CharField(max_length=5,blank=True,null=True)
+    big5_code = models.CharField(max_length=5,blank=True,null=True)
